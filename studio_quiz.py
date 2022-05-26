6377f99c463835cb8c1726f514a98d83cfe1e1d5
Ivan Bogomolov <vanya@abogomolov.com>
2022-05-26 20:16:00 +0300

    update text

--- studio_quiz.py
+++ studio_quiz.py
@@ -1,32 +1,16 @@
-##is_dark = input('На улице темно? да/нет\n')
-##if is_dark=='да':
-##    print('Спокойной ночи! Хрррррррррррррр')
-##    
-
-
-##joker=input('Ты шут?да?/нет\n')
-##if joker=='да':
-##    print('Ну и ну Джевил за компьютером!')
-##else:
-##    print('Приветствую тебя, человек!')
-
-
-studio=input('Какая студия вам по душе?\n'
-             '1:Klei Entertainment\n'
-             '2:Toby Fox\n'
-             '3:Team Cherry\n'
-             '(отвечайте 1,2 или 3)\n'
-             )
-if studio=='1':
-    print('Оооооо Klei Entertement, они создали великие:Dont starve, и великолепный Oxygen not included')
-elif studio=='2':
-    print('Оооо Toby Fox, он создал: Undertale и Deltarune')
+studio = input(
+    "Какая студия вам по душе?\n"
+    "1:Klei Entertainment\n"
+    "2:Toby Fox\n"
+    "3:Team Cherry\n"
+    "(отвечайте 1,2 или 3)\n"
+)
+if studio == "1":
+    print(
+        "Оооооо Klei Entertement, они создали великие игры:\n"
+        "Dont starve, и великолепный Oxygen not included"
+    )
+elif studio == "2":
+    print("Оооо Toby Fox, он создал: Undertale и Deltarune")
 else:
-    print('Ооо Team Сhrry они создали Hollow Knight')
-    
-
-          
-          
-          
-    
-
+    print("Ооо Team Сhrry они создали Hollow Knight")
