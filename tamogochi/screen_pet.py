1284bd37ff91fd653d164c7780c167afb6c900dc
Ivan Bogomolov <vanya@abogomolov.com>
2022-06-09 16:37:23 +0300

    initial commit

---
+++ tamogochi/screen_pet.py
@@ -0,0 +1,11 @@
+# сделать интерактивного питомца.
+
+
+from logging import root
+from tkinter import HIDDEN, NORMAL, Tk, Canvas
+root = Tk()
+c = Canvas(root, width=400, height=400)
+c.configure(bg='dark blue', highlightthickness=0)
+c.body_color = 
+c.pack()
+root.mainloop()
