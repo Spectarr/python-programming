ccce6dff68b31e41f65faf7f757d2682155c1795
Ivan Bogomolov <vanya@abogomolov.com>
2023-04-22 00:35:16 +0300

    add gui class

--- coord_draw/coord_draw.py
+++ coord_draw/coord_draw.py
@@ -12,9 +12,6 @@ from tkinter import (
 )
 import turtle as t
 
-root = Tk()
-root.withdraw()
-
 
 def prepare_instructions(raw_instructions: list, scale: int):
     # remove spaces, newlines and ending symbols
@@ -65,16 +62,10 @@ def draw(coords):
         t.goto(get_xy(coord))
 
 
-def on_closing():
-    win.destroy()
-    sys.exit()
-
-
 def process(scale, instructions=None):
     if not instructions:
         messagebox.showinfo("Warning", "No instructions found, add some")
         return
-    root.quit()
     instructions = prepare_instructions(instructions, scale)
 
     t.shape("arrow")
@@ -90,58 +81,69 @@ def process(scale, instructions=None):
 
 
 def parse_instructions(text: str) -> list:
-    instructions = []
+    result = []
     messages = text.split("и")
     for message in messages:
         # remove all cyrillic symbols, dots and colons
-        message = re.sub("[а-я|А-Я]|:|\.", "", message)
-        instructions.append(message)
-    return instructions
+        message = re.sub("[а-я|А-Я]|:", "", message)
+        result.append(message)
+    return result
 
 
-def add():
-    global instructions
-    message = text_box.get("1.0", "end-1c")
-    print(f"adding: {message}")
+class GUI:
+    def __init__(self) -> None:
+        self.instructions = []
+        self.win = Tk()
+        self.win.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.win.geometry("350x200+560+200")
+        self.win.title("Нарисуй по точкам")
+        self.text_box = Text(
+            self.win,
+            height=5,
+            width=48,
+            background="light grey",
+            border=3,
+            font=("Courier", 8),
+        )
+        button_add = Button(self.win, text="Add", command=self.add)
+        slider_label = Label(self.win, text="Scale")
+        self.scale_slider = Scale(
+            self.win, from_=10, to=40, length=220, tickinterval=10, orient=HORIZONTAL
+        )
+        self.scale_slider.set(20)
+        button_draw = Button(
+            self.win,
+            text="Draw",
+            command=lambda: process(self.scale_slider.get(), self.instructions),
+        )
+        button_clear = Button(self.win, text="Clear", command=self.clear_data)
+        button_exit = Button(self.win, text="Exit", command=quit)
+        self.label_status = Label(self.win, text="")
+        self.text_box.grid(column=0, columnspan=4, row=0)
+        button_add.grid(column=0, row=1)
+        button_draw.grid(column=1, row=1)
+        button_clear.grid(column=2, row=1)
+        button_exit.grid(column=3, row=1)
+        slider_label.grid(column=0, row=2)
+        self.scale_slider.grid(columnspan=3, column=1, row=2)
+        self.label_status.grid(columnspan=4, column=0, row=3)
+        self.win.mainloop()
 
-    instructions = parse_instructions(message)
-    label.config(text=f"added {len(instructions)} elements")
-    text_box.delete("1.0", END)
+    def add(self):
+        message = self.text_box.get("1.0", "end-1c")
+        parsed = parse_instructions(message)
+        self.instructions.extend(parsed)
+        self.label_status.config(text=f"added {len(self.instructions)} elements")
+        self.text_box.delete("1.0", END)
 
+    def clear_data(self):
+        self.instructions = []
+        self.label_status.config(text=f"data cleared")
 
-def clear_data():
-    global instructions
-    instructions = []
-    label.config(text=f"data cleared")
+    def on_closing(self):
+        self.win.destroy()
+        sys.exit()
 
 
 if __name__ == "__main__":
-    win = Tk()
-    win.protocol("WM_DELETE_WINDOW", on_closing)
-    win.geometry("350x200+560+200")
-    win.title("Нарисуй слона")
-    text_box = Text(
-        win, height=5, width=48, background="light grey", border=3, font=("Courier", 8)
-    )
-    instructions = []
-    button_add = Button(win, text="Add", command=add)
-    slider_label = Label(win, text="Scale")
-    scale_slider = Scale(
-        win, from_=10, to=40, length=220, tickinterval=10, orient=HORIZONTAL
-    )
-    scale_slider.set(20)
-    button_draw = Button(
-        win, text="Draw", command=lambda: process(scale_slider.get(), instructions)
-    )
-    button_clear = Button(win, text="Clear", command=clear_data)
-    button_exit = Button(win, text="Exit", command=quit)
-    label = Label(win, text="")
-    text_box.grid(column=0, columnspan=4, row=0)
-    button_add.grid(column=0, row=1)
-    button_draw.grid(column=1, row=1)
-    button_clear.grid(column=2, row=1)
-    button_exit.grid(column=3, row=1)
-    slider_label.grid(column=0, row=2)
-    scale_slider.grid(columnspan=3, column=1, row=2)
-    label.grid(columnspan=4, column=0, row=3)
-    win.mainloop()
+    gui = GUI()
